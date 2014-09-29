02c975258d8e3ce2d0ce6b9aa7e7c4040118e083
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-09-29 10:48:13 +0200

    new UI for backend

---
+++ Backend/Server/DataPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class DataPoint
+    {
+        double _lat, _lon;
+
+        public DataPoint(double lat, double lon)
+        {
+            _lat = lat;
+            _lon = lon;
+        }
+
+        public double GetLat()
+        {
+            return _lat;
+        }
+
+        public double GetLon()
+        {
+            return _lon;
+        }
+
+    }
+}
---
+++ Backend/Server/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelServerStatus.Text = "Stopped.";
+        }
+
+        private void buttonLaunch_Click(object sender, EventArgs e)
+        {
+            
+            PositionListener listener = new PositionListener();
+
+            if (textBoxPort.Text == "")
+            {
+                Thread myNewThread = new Thread(() => listener.Start(8000));
+                myNewThread.Start();
+            }
+            else
+            {
+                Thread myNewThread = new Thread(() => listener.Start(Convert.ToInt32(textBoxPort.Text)));
+                myNewThread.Start();
+            }
+
+            labelServerStatus.Text = "Running on port " + (textBoxPort.Text == "" ? "8000" : "Running on port " + textBoxPort.Text);
+        }
+    }
+}
