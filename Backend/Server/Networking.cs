66b6d4cd806e892c8bceffaf486055d830195f29
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-10-27 10:52:27 +0100

    forgot to commit. networking stuff and extra form to show data received

---
+++ Backend/Server/Networking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Networking
+    {
+        public static void SendData(string data, string ip, int port = 8080)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.SendTimeout = 1000;
+                client.Connect(ip, port);
+                NetworkStream stream_out = client.GetStream();
+
+                // send data
+                stream_out.Write(Encoding.ASCII.GetBytes(data), 0, data.Length);
+
+                // flush to make sure data has been sent
+                stream_out.Flush();
+
+                // clean up
+                stream_out.Close();
+                System.Windows.Forms.MessageBox.Show("Sent message.");
+            }
+            catch (Exception)
+            {
+                System.Windows.Forms.MessageBox.Show("Ooooops.. Something went wrong.");
+            }
+        }
+    }
+}
