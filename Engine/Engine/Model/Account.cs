7f0e77b6177425ca4fe438ce782c665e65f47be9
Kristian <kthoms10@student.aau.dk>
2014-11-20 14:19:57 +0100

    account model

--- Engine/Engine/Model/Account.cs
+++ Engine/Engine/Model/Account.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Engine
 {
 	public class Account
 	{
-		public Account ()
+		public int id {
+			get { return id; }
+		}
+		public string userName { 
+			get { return userName; }
+		}
+		public List<Player> players	{
+			get { return players; }
+			set { players = value; }
+		}
+		public List<Game> hosting {
+			get { return hosting; }
+			set { hosting = value; }
+		}
+
+		public Account (int id, string userName)
 		{
+			id = id;
+			userName = userName;
 		}
 	}
 }
