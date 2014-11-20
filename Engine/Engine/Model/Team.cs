24f5530b2ffeb49032d25e825455e478bc099539
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-11-20 15:18:14 +0100

    accessors for Team

--- Engine/Engine/Model/Team.cs
+++ Engine/Engine/Model/Team.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Engine
 {
 	public class Team
 	{
-		public Team ()
+        private int _id, _score;
+        private List<Player> _teamMembers;
+
+		public Team (int id)
 		{
+            _id = id;
+            _teamMembers = new List<Player>();
 		}
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
 	}
 }
 
