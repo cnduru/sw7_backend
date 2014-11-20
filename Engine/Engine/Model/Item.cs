dd2fe14d3c4576c05867d0f50b29419a1d63ee23
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-11-20 15:30:39 +0100

    accessors for Item

--- Engine/Engine/Model/Item.cs
+++ Engine/Engine/Model/Item.cs
@@ -4,9 +4,29 @@ namespace Engine
 {
 	public class Item
 	{
-		public Item ()
+        private int _id, _itemType, _effect;
+
+		public Item (int id, int itemType, int effect)
 		{
+            _id = id;
+            _itemType = itemType;
+            _effect = effect;
 		}
+
+        public int id
+        {
+            get { return _id; }
+        }
+
+        public int itemType
+        {
+            get { return _itemType; }
+        }
+
+        public int effect
+        {
+            get { return _effect; }
+        }
 	}
 }
 
