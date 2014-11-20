9d6a95ab2ca27a016f132b19c521df199f0c4699
Kristian <kthoms10@student.aau.dk>
2014-11-20 14:49:48 +0100

    better now?

--- Engine/Engine/Model/Account.cs
+++ Engine/Engine/Model/Account.cs
@@ -5,27 +5,31 @@ namespace Engine
 {
 	public class Account
 	{
-		public int id {
-			get { return id; }
-			set { id = value; }
-		}
-		public string userName { 
-			get { return userName; }
-			set { userName = value; }
-		}
-		public List<Player> players	{
-			get { return players; }
-			set { players = value; }
-		}
-		public List<Game> hosting {
-			get { return hosting; }
-			set { hosting = value; }
+		private int _id;
+		private string _userName;
+		private List<Player> _players;
+		private List<Game> _hosting;
+
+		public Account (int id, string userName)
+		{
+			_id = id;
+			_userName = userName;
 		}
 
-		public Account (int _id, string _userName)
-		{
-			id = _id;
-			userName = _userName;
+		//Accessors
+		public int id {
+			get { return _id; }
+		}
+		public string userName { 
+			get { return _userName; }
+		}
+		public List<Player> players	{
+			get { return _players; }
+			set { _players = value; }
+		}
+		public List<Game> hosting {
+			get { return _hosting; }
+			set { _hosting = value; }
 		}
 	}
 }
