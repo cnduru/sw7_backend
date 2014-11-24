3a6990b5ffb46b9c2e050dfafcd310f948b4dd36
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-11-24 13:44:03 +0100

    dispatcher init

---
+++ Backend/Server/Dispatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Dispatcher
+    {
+        public void Dispatch(string xml)
+        {
+            if (xml.Contains("<Login>"))
+            {
+                // todo: this string should be sent back to the android client
+                string authStatus = XMLhandler.AuthenticateLogin(xml);
+            }
+        }
+    }
+}
