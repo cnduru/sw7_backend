aa10503d8225378af0104bf4eca04df065f3820c
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-11-24 13:58:31 +0100

    Documentation comment and else clause

--- Backend/Server/Dispatcher.cs
+++ Backend/Server/Dispatcher.cs
@@ -6,14 +6,23 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class Dispatcher
+    static class Dispatcher
     {
-        public void Dispatch(string xml)
+        /// <summary>
+        /// Method dispatching XML instruction to the right component and returns a status XML string
+        /// </summary>
+        public static string Dispatch(string xml)
         {
             if (xml.Contains("<Login>"))
             {
                 // todo: this string should be sent back to the android client
                 string authStatus = XMLhandler.AuthenticateLogin(xml);
+                return authStatus;
+            }
+            else
+            {
+                // no tag matched a known dispatch instruction
+                return @"<Error>Message not understood</Error>";
             }
         }
     }
