0b85effd1757b7e6a5b70c38065c70bb9cff830e
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-11-25 10:40:08 +0100

    Skeleton for dispatcher and user authentification

---
+++ Engine/Engine/Dispatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    static class Dispatcher
+    {
+        /// <summary>
+        /// Method dispatching XML instruction to the right component and returns a status XML string
+        /// </summary>
+        public static string Dispatch(string xml)
+        {
+            XMLhandler xh = new XMLhandler();
+
+            if (xml.Contains("<Login>"))
+            {
+                // todo: this string should be sent back to the android client
+                string[] loginData = xh.GetLoginData(xml);
+                string response = Auth.VerifyAccount(loginData[0], loginData[1]);
+                return response;
+            }
+            else
+            {
+                // no tag matched a known dispatch instruction
+                return @"<Error>Message not understood</Error>";
+            }
+        }
+    }
+}
---
+++ Engine/Engine/XMLhandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Engine
+{
+    class XMLhandler
+    {
+        ///<Summary>
+        /// Method for extracting login data from XML 
+        ///</Summary>
+        public string[] GetLoginData(string xml) //string path = "login.xml")
+        {
+            string pattern = @"<Login><Username>([a-zA-Z0-9]*)<\S*<Password>([a-zA-Z0-9]*)<";
+
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = rgx.Matches(xml);
+
+            bool first = true;
+            string[] res = {"", ""};
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    res[0] = match.Groups[1].Value;
+                    res[1] = match.Groups[2].Value;
+                }
+                    
+            }
+
+            return res;
+        }
+    }
+
+  
+}
