210cfad86124e268a45644a2e2afe42ece841516
Casper Laustsen <casper.laustsen@gmail.com>
2014-12-02 10:38:11 +0100

    gameevent og gamethread coded. Har dummy XML returns til dispatcher har også global timer

--- Engine/Engine/GlobalTimerThread.cs
+++ Engine/Engine/GlobalTimerThread.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Engine {
     class GlobalTimerThread {
-        Queue<GameEvent> gameEvents = new Queue<GameEvent>();
-        
+        private static List<GameEvent> gameEvents = new List<GameEvent>();
         public GlobalTimerThread() {
 
         }
 
-        public void enqueueGameEvent(GameEvent gameEvent) {
-            gameEvents.Enqueue(gameEvent);
+        public static void Run() {
+            Console.WriteLine("Global Timer initialised.");
+            while (true) {
+                if (gameEvents.Count == 0) {
+                    Thread.Yield();
+                } else {
+                    for (int i = 0; i < gameEvents.Count(); i++) {
+                        if (gameEvents[i].GetTriggerTimeStamp() < DateTime.Now) {
+                            gameEvents[i].RunGameEvent();
+                            try {
+                                gameEvents.RemoveAt(i);
+                            } catch (Exception e) {
+                                throw new ArgumentOutOfRangeException("attempted to remove gameEvent at i, no succes");
+                            }
+                           break;
+                        }
+                    }
+                }
+            }
         }
 
-        public GameEvent dequeueGameEvent() {
-            return gameEvents.Dequeue();
+        public static void AddGameEvent(GameEvent gameEvent) {
+            gameEvents.Add(gameEvent);
         }
 
 
